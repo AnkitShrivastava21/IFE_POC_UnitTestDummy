f38013d8ac4f225e2296941b90c53d8188d8be0d
AnkitShrivastava21 <30997103+AnkitShrivastava21@users.noreply.github.com>
2017-09-06 09:16:00 +0200

    Uploaded Data Driven Project  all Files

---
+++ DataDrivenDummy/DataDrivenTest/DataDrivenTest/TargetViewDataDriven.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataDrivenTest
+{
+    [TestClass]
+    public class TargetViewDataDriven
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [DeploymentItem("TestData.csv")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\TestData.csv", "TestData#csv",
+         DataAccessMethod.Sequential)]
+
+        [TestMethod]
+        public void DataDrive()
+        {
+            string CorpID = Convert.ToString(TestContext.DataRow[0].ToString());
+            string Name = Convert.ToString(TestContext.DataRow[1].ToString());
+            Console.WriteLine("Datasource Corp ID "  + CorpID);
+            Console.WriteLine("Datasource Name " + Name);
+
+            Trace.WriteLine("Input Corp ID " + CorpID);
+            Trace.WriteLine("Input Name " + Name);
+        }
+    }
+}
